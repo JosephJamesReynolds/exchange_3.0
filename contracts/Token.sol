35536f0c5117c3676687c15d61b588ce686b5b35
Joseph <josephreynoldsblockchain@protonmail.com>
2025-06-20 16:29:33 -0700

    Constructor

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -6,4 +6,12 @@ contract Token {
     string public symbol = "RTK";
     uint8 public decimals = 18;
     uint256 public totalSupply = 20_000_000 * (10 ** decimals);
+
+constructor(string memory name_, string memory symbol_, uint8 decimals_, uint256 totalSupply_) {
+    name = name_;
+    symbol = symbol_;
+    decimals = decimals_;
+    totalSupply = totalSupply_;
+}
+
 }
