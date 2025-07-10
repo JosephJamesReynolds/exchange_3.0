7b367aa15932dbb91b9dfc93f3f94b14ad8dc75d
Joseph <josephreynoldsblockchain@protonmail.com>
2025-07-09 21:37:20 -0700

    Started Exchange Contract

---
+++ contracts/Exchange.sol
@@ -0,0 +1,4 @@
+// SPDX-License-Identifier: UNLICENCED
+pragma solidity ^0.8.28;
+
+contract Exchange {}
---
+++ test/Exchange.js
@@ -0,0 +1,21 @@
+const {
+  loadFixture,
+} = require("@nomicfoundation/hardhat-toolbox/network-helpers");
+const { expect } = require("chai");
+const { ethers } = require("hardhat");
+
+const { deployTokenFixture } = require("./helpers/TokenFixtures.js");
+
+const tokens = (n) => {
+  return ethers.parseUnits(n.toString(), 18);
+};
+
+describe("Exchange", () => {
+  //   beforeEach(async () => {
+  //     ({ token, deployer, receiver, exchange, user1, user2 } =
+  //       await loadFixture(deployTokenFixture));
+  //   });
+  describe("Deployment", () => {
+    it("", async () => {});
+  });
+});
